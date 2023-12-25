0369c98870396bf576d24c3ac0f1a3da559f1f07
Replit user <>
2023-12-25 20:50:49 +0000

    update db credentials

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 
-usr = '37i3djj7iynnncqf0er1'
-pwd = 'pscale_pw_MOZsp0q6pht1TB4uHJzNPnNUjh3u7D9PSrOC2l2rkm6'
-host = 'aws.connect.psdb.cloud'
+usr = 'duoz5ozsyzetvzlksz2c'
+pwd = 'pscale_pw_x5FU5Jifh4r6FuCAMwGCYaFdC43NNaz0iTQ9s97xPIm'
+host = 'us-east.connect.psdb.cloud'
 db = 'tours'
 
 connection_string = "mysql+pymysql://" + usr + ":" + pwd + "@" + host + ":3306/" + db
