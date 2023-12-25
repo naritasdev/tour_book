7124ea28494c58d8488d24a1d3c34447cb09f7ec
Humberto Yances <hyances@gmail.com>
2023-12-25 15:24:07 -0500

    update db credentials

--- database.py
+++ database.py
@@ -1,6 +1,11 @@
 from sqlalchemy import create_engine
 
-connection_string = "mysql+pymysql://xqfwvqq5cu6lbnn58sau:pscale_pw_V2GUBhUORuf1JtIJEE6fHg2UrdsAWP8yVDgVO6bc7kp@aws.connect.psdb.cloud:3306/tours"
+usr = '37i3djj7iynnncqf0er1'
+pwd = 'pscale_pw_MOZsp0q6pht1TB4uHJzNPnNUjh3u7D9PSrOC2l2rkm6'
+host = 'aws.connect.psdb.cloud'
+db = 'tours'
+
+connection_string = "mysql+pymysql://" + usr + ":" + pwd + "@" + host + ":3306/" + db
 engine = create_engine(connection_string, 
                        echo=False,
                        connect_args={
