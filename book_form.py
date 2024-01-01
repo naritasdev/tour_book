0f93b5e368b015e6494493507726b9fadc9d7d6b
danielpajaro <daniel.pajaro98@gmail.com>
2024-01-01 14:55:33 -0500

    #TOUR-8 Almacenar los tours solicitados en una base de datos

--- book_form.py
+++ book_form.py
@@ -41,7 +41,7 @@ def load_request_from_db(request_id): # or request_name
 
 def add_request_from_db(request_name, request_email, request_date, request_requests): 
     with engine.connect() as conn:
-        result = conn.execute(text("insert into request (request_name,request_email,request_date,request_requests) values (%s, %s, %s) ", (request_name,request_email,request_date,request_requests)))
+        result = conn.execute(text("insert into request (request_name,request_email,request_date,request_requests) values (%s, %s, %s, %s) ", (request_name,request_email,request_date,request_requests)))
         
 
 
